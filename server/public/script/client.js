0f5f14b5cc779c60dfda883ff6fa6c15d6b6e6d4
Alaethia <truthcharis@gmail.com>
2017-12-21 15:01:50 -0600

    added post

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -1,1 +1,57 @@
 console.log('js sourced');
+
+$(document).ready(start);
+//configuring request
+function start(){
+    //event listeners
+    $('#addNewGift').on('click', addNewGift);
+    // canSantaFly();
+} //end start function
+
+function addNewGift() {
+    console.log('adding new gift');
+    let newGift = $('#giftName').val();
+    console.log('adding new gift', newGift);
+
+    $.ajax({
+        method: 'POST',
+        url: '/gifts',
+        data: { newGiftProperty: newGift},//expects an object 
+        success: function(response){
+            console.log('added new gift', response);   
+        },//end ajax
+
+        error: function(response){
+            console.log('error adding gift', response);
+            
+        }//end error
+     });
+    
+    //calling input and getting the input off of that value
+
+}//end add gift function 
+
+function canSantaFly(){
+    $.ajax({
+        method: 'GET',
+        url: '/santa',
+        // request went to the server...
+        success: function(response){
+            //res.send becomes response 
+            console.log('get /santa response', response);
+            getGifts();
+        }//end success function 
+    }) //end ajax method
+}//end santa can fly function 
+
+function getGifts(){
+    $.ajax({
+        method: 'GET',
+        url: '/gifts',
+        // request went to the server...
+        success: function(response){
+            //res.send becomes response 
+            console.log('get /gifts response', response);  
+        }//end success function 
+    }) //end ajax method
+}//end get gifts function 
--- server/server.js
+++ server/server.js
@@ -2,17 +2,24 @@
 const express = require( 'express' );
 // without require there is no file path 
 const app = express();
+const bodyParser = require('body-parser');
 
+let gifts = ['hat', 'skates',];
 //define middleware
 app.use(express.static('server/public'));
-
+app.use(bodyParser.urlencoded());
 //define routes 
 // app.get, app.post........
 //app.use -> all request methods 
 app.get('/gifts', function(req, res) {
-    let gifts = ['hat', 'skates',];
     res.send(gifts);
 });
+
+app.post('/gifts', function(req, res){
+    console.log('body:', req.body);//data becomes req.body
+    res.sendStatus(201);
+    
+})
 app.get('/santa', function(req, res){
     red.send('santa can fly');
 });
